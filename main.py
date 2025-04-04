22bcba0059b849e3b3c3152abc361d96e07099e9
Davison Rebechi <davison.rebechi@outlook.com>
2025-04-04 11:50:18 -0300

    removendo arquivo main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from google.cloud import storage, bigquery
-
-def hello_gcp():
-    print("✅ Ambiente GCP configurado!")
-    print("→ Storage client:", storage.Client())
-    print("→ BigQuery client:", bigquery.Client())
-
-if __name__ == "__main__":
-    hello_gcp()
